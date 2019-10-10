05639c7305f2581158c3667d279dba956d68a725
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-10-10 00:08:43 -0400

    created the rest of the database methods and tested all endpoints on insomnia. MVP reached

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,10 +3,10 @@ const db = require('../data/db-config.js');
 module.exports = {
     find,
     findById,
-    // findSteps,
-    // add,
-    // update,
-    // remove
+    findSteps,
+    add,
+    update,
+    remove
 };
 
 function find() {
@@ -17,4 +17,40 @@ function findById(id) {
     return db('schemes')
         .where({ id })
         .first();
+}
+
+function findSteps(id) {
+    return db('schemes')
+        .join('steps', 'schemes.id', '=', 'steps.scheme_id')
+        .select(
+            'steps.id',
+            'schemes.scheme_name',
+            'steps.step_number',
+            'steps.instructions'
+        )
+        .where('schemes.id', id)
+        .orderBy('steps.step_number', 'asc');
+}
+
+function add(scheme) {
+    return db('schemes')
+        .insert(scheme, 'id')
+        .then(([id]) => {
+            return findById(id);
+        });
+}
+
+function update(changes, id) {
+    return db('schemes')
+        .update(changes)
+        .where({ id })
+        .then(() => {
+            return findById(id);
+        });
+}
+
+function remove(id) {
+    return db('schemes')
+        .where({ id })
+        .del();
 }
